2933a49a9bf04caba86ad5499005d14ca52eba48
karan joshi <kjkaranjoshi2006@gmail.com>
2026-03-06 17:19:43 +0530

    LEARNING ABOUT VITE PROJECTS & IT'S COMMANDS

---
+++ demo/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+// EXPORT DEFAULT FUNCTION NAME . 
+export default App
+
+
+// exporrt default function name ==> 
+  // export default function App() {
--- karanportfolio/src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  return (
-    <h1>karan joshi</h1>
-  );
-}
-
-export default App;
