a3d602dc88b2db632224970efe5ba7e729ca3cde
karan joshi <kjkaranjoshi2006@gmail.com>
2026-03-08 19:13:47 +0530

    CSS FOLDER STRUCTURE

--- demo/src/components/Topbar/TopBar.jsx
+++ demo/src/components/Topbar/TopBar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import "../styles/TopBar.css"
 
 const TopBar = () => {
   return (
-    <div>
+    <div className="main-container" id='maincon'>
       <h1>radhe radhe</h1>
       <h1>radhe shyam</h1>
     </div>
