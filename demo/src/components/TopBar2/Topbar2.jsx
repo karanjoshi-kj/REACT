1be7e3653e6cc9772d62ccd23f58ef8dbb3c7103
karan joshi <kjkaranjoshi2006@gmail.com>
2026-03-09 18:08:19 +0530

    LEARN HOW TO APPLY THE REACT ICONS & CREATE A SIMPLE TOPBAR BASED ON TODAY'S UNDERSTANDING

--- demo/src/components/TopBar2/Topbar2.jsx
+++ demo/src/components/TopBar2/Topbar2.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import karan1 from "../styles/Topbar2.module.css"
 import { IoLocationSharp , IoMail } from "react-icons/io5";
 import { FaPhone , FaInstagram , FaLinkedin} from "react-icons/fa6";
 import { FaFacebookSquare , FaTwitter} from "react-icons/fa";
 const Topbar2 = () => {
   return (
-    <div className='Topbar-eg'>
-      <div className='leftpart'>
+    <div className={karan1.Topbareg}>
+      <div className={karan1.leftpart}>
         <span><IoLocationSharp />Location</span>
         <span><FaPhone />+13122323213</span>
         <span><IoMail />abc@gmail.com</span>
       </div>
-      <div className='rightpart'>
+      <div className={karan1.rightpart}>
         <span><FaFacebookSquare /></span>
         <span><FaTwitter /></span>
         <span><FaInstagram /></span>
