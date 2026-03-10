f0c02e63fcaa7195e5648aa96a30ab30ed034c29
karan joshi <kjkaranjoshi2006@gmail.com>
2026-03-10 17:47:31 +0530

    QUESTION PRATICE OF CREATING A NAVBAR

--- demo/src/components/NavBar2/navbar2.jsx
+++ demo/src/components/NavBar2/navbar2.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import hello from "../styles/Navbar2.module.css";
+import { FaInstagram , FaLinkedin} from "react-icons/fa6";
+import { FaFacebookSquare , FaTwitter} from "react-icons/fa";
 const Navbar2 = () => {
   return (
-    <div>
-      <div className='leftpart'>
+    <div className={hello.complete}>
+      <div className={hello.leftpart}>
         <h2>startup 3</h2>
       </div>
-      <div className='middlepart'>
+      <div className={hello.middlepart}>
         <span>Tour</span>
         <span>Features</span>
         <span>Pricing</span>
         <span>Blog</span>
+        <span>
+          <FaFacebookSquare />
+        </span>
+        <span>
+          <FaTwitter />
+        </span>
+        <span>
+          <FaInstagram />
+        </span>
+        <span>
+          <FaLinkedin />
+        </span>
       </div>
-      <div className='rightpart'>
-        <input type='button'>Buy on app store</input>
+      <div className={hello.rightpart}>
+        <button>Buy on app store</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar2
+export default Navbar2;
