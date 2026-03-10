e67b31951f8209233a02fd9b1add8b8ed5b33d48
karan joshi <kjkaranjoshi2006@gmail.com>
2026-03-10 18:11:47 +0530

    QUESTION PRATICE OF CREATING A NAVBAR

--- demo/src/components/navbar3/Navbar3.jsx
+++ demo/src/components/navbar3/Navbar3.jsx
@@ -1,23 +1,36 @@
 import React from 'react'
+import obj from "../styles/Navbar3.module.css"
+import { FaSearch , FaHome , FaFlag , FaVideo , FaShoppingBag , FaPlus , } from "react-icons/fa";
+import { IoPeopleSharp } from "react-icons/io5";
+import { LuMessageCircleMore } from "react-icons/lu";
+import { IoMdNotifications } from "react-icons/io";
 
 const Navbar3 = () => {
   return (
-    <div>
-      <div className='leftpart'>
+    <div className={obj.complete}>
+      <div className={obj.leftpart}>
         <h1>M</h1>
         <h1>D</h1>
         <h1>B</h1>
-        <div className='searchbox'>
+        <span><FaSearch /></span>
+        <div className={obj.searchbox}>
             <input type='text' placeholder='search'/>
         </div>
       </div>
 
-      <div className='middlepart'>
-
+      <div className={obj.middlepart}>
+        <span><FaHome /></span>
+        <span><FaFlag /></span>
+        <span><FaVideo /></span>
+        <span><FaShoppingBag /></span>
+        <span><IoPeopleSharp /></span>
       </div>
 
-      <div className='rightpart'>
+      <div className={obj.rightpart}>
         <h3>jhon</h3>
+        <span><FaPlus /></span>
+        <span><LuMessageCircleMore /></span>
+        <span><IoMdNotifications /></span>
       </div>
     </div>
   )
