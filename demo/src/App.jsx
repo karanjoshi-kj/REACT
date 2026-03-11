a159bef110780f0ddd4438f286e98977130de982
karan joshi <kjkaranjoshi2006@gmail.com>
2026-03-11 18:37:46 +0530

    LEARN HOW TO APPLY MULTIPLE CSS IN A SINGLE JSX FILE

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -2,13 +2,15 @@
 // import NavBar from "./components/Topbar/NavBar"
 // import Topbar2 from "./components/TopBar2/Topbar2"
 
-import Navbar3 from "./components/navbar3/Navbar3"
+// import Navbar3 from "./components/navbar4/Navbar3"
+
+import Navbar4 from "./components/navbar4/Navbar4"
 
 
 function App() {
   return (
     <>
-    <Navbar3 />
+    <Navbar4 />
     {/* <Navbar2 />       */}
       {/* <Topbar2 /> */}
       {/* <NavBar /> */}
--- demo/src/components/navbar4/Navbar4.jsx
+++ demo/src/components/navbar4/Navbar4.jsx
@@ -1,39 +1,17 @@
 import React from 'react'
-import obj from "../styles/Navbar3.module.css"
-import { FaSearch , FaHome , FaFlag , FaVideo , FaShoppingBag , FaPlus , } from "react-icons/fa";
-import { IoPeopleSharp } from "react-icons/io5";
-import { LuMessageCircleMore } from "react-icons/lu";
-import { IoMdNotifications } from "react-icons/io";
-
-const Navbar3 = () => {
+import obj from "../styles/Navbar4.module.css"
+const Navbar4 = () => {
   return (
-    <div className={obj.complete}>
-      <div className={obj.leftpart}>
-        <h1>M</h1>
-        <h1>D</h1>
-        <h1>B</h1>
-        <span><FaSearch /></span>
-        <div className={obj.searchbox}>
-            <input type='text' placeholder='search'/>
-        </div>
-      </div>
+    <div>
+        {/* FIRST METHOD TO APPLY MULTIPLE CSS IS STRING LITERALS */}
+      <ul className={`${obj.firstcss} ${obj.secondcss}`}  />
 
-      <div className={obj.middlepart}>
-        <span><FaHome /></span>
-        <span><FaFlag /></span>
-        <span><FaVideo /></span>
-        <span><FaShoppingBag /></span>
-        <span><IoPeopleSharp /></span>
-      </div>
-
-      <div className={obj.rightpart}>
-        <h3>jhon</h3>
-        <span><FaPlus /></span>
-        <span><LuMessageCircleMore /></span>
-        <span><IoMdNotifications /></span>
-      </div>
+      <li>About</li>
+      <li>Contact</li>
+      <li>Carrer</li>
+      <li>Services</li>
     </div>
   )
 }
 
-export default Navbar3
+export default Navbar4
