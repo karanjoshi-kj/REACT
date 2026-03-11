c52ede39f14c53835884ebfe30fd9df52fb9333c
karan joshi <kjkaranjoshi2006@gmail.com>
2026-03-11 18:46:01 +0530

    LEARN HOW TO APPLY MULTIPLE CSS IN A SINGLE JSX FILE

--- demo/src/components/navbar4/Navbar4.jsx
+++ demo/src/components/navbar4/Navbar4.jsx
@@ -3,8 +3,11 @@ import obj from "../styles/Navbar4.module.css"
 const Navbar4 = () => {
   return (
     <div>
-        {/* FIRST METHOD TO APPLY MULTIPLE CSS IS STRING LITERALS */}
+        {/* FIRST METHOD TO APPLY MULTIPLE CSS IS STRING LITERALS . */}
       <ul className={`${obj.firstcss} ${obj.secondcss}`}  />
+
+        {/* SECOND METHOD TO APPLY MULTIPLE CSS IS JOIN METHOD . */}
+      <li className={[obj.licolor , obj.listyle].join("")}>home</li>
 
       <li>About</li>
       <li>Contact</li>
