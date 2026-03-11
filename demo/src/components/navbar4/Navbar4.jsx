9f48b9ac926f7b78f5b16a58e4e0a0078d4e0d50
karan joshi <kjkaranjoshi2006@gmail.com>
2026-03-11 18:58:42 +0530

    LEARN HOW TO APPLY MULTIPLE CSS IN A SINGLE JSX FILE & IMPORTANT CONCEPT OF CLASSNAME

--- demo/src/components/navbar4/Navbar4.jsx
+++ demo/src/components/navbar4/Navbar4.jsx
@@ -7,10 +7,13 @@ const Navbar4 = () => {
       <ul className={`${obj.firstcss} ${obj.secondcss}`}  />
 
         {/* SECOND METHOD TO APPLY MULTIPLE CSS IS JOIN METHOD . */}
-        
-      <li className={[obj.licolor , obj.listyle].join("")}>home</li>
 
-      <li>About</li>
+      {/* <li className={[obj.licolor , obj.listyle].join("")}>home</li> */}
+
+      {/* IF WE WANT TO GIVE ( - ) DESH IN THE CLASS NAME  */}
+      <li className={obj["li-color"]}>home</li>
+      <li className={`${obj["listyle"]} ${obj["li-color"]}`}>about</li>
+
       <li>Contact</li>
       <li>Carrer</li>
       <li>Services</li>
