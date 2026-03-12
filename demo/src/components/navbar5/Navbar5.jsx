698ab44f7895aeb1fbb2b415fee937e153768f22
karan joshi <kjkaranjoshi2006@gmail.com>
2026-03-12 18:32:56 +0530

    LEARN HOW TO USE THE INTERNAL JSON IN THE JSX FILE

---
+++ demo/src/components/navbar5/Navbar5.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import obj from "../styles/Navbar4.module.css"
+
+
+//! TAKING THE CONTENT IN THE JSON FORMAT . 
+
+
+
+const navabardata = {
+    ABOUT : "ABOUT",
+    HOME : "HOME",
+    CONTACT : "CONTACT",
+    SERVICES : "SERVICES",
+    CARRER : "CARRER"
+};
+
+
+
+
+
+const Navbar4 = () => {
+  return (
+    <div>
+      <ul className={`${obj.firstcss} ${obj.secondcss}`}  />
+
+      <li className={obj["li-color"]}>{navabardata.HOME}</li>
+      <li className={`${obj["listyle"]} ${obj["li-color"]}`}>{navabardata.ABOUT}</li>
+
+      <li>{navabardata.CONTACT}</li>
+      <li>{navabardata.SERVICES}</li>
+      <li>{navabardata.CARRER}</li>
+    </div>
+  )
+}
+
+export default Navbar4
