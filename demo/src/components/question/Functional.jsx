7d99e4f2f72e907b5eb936a3bfc85e084e173962
karan joshi <kjkaranjoshi2006@gmail.com>
2026-03-14 17:48:52 +0530

    QUESTION PRATICE ON FUNCTIONAL & DEFAULT COMPONENTS IN REACT

--- demo/src/components/question/Functional.jsx
+++ demo/src/components/question/Functional.jsx
@@ -2,26 +2,26 @@ import obj from "../question/Button.module.css"
 
 export function Backtotop(){
     return(
-        <button onClick='#' className={obj.backtotop}>BACK TO TOP</button>
+        <button onClick='#' className={`${obj.backtotop} ${obj.globalbtn}`}>BACK TO TOP</button>
     )
 }
 export function Backtohome(){
     return(
-        <button onClick='#' className={obj.backtohome}>BACK TO HOME</button>
+        <button onClick='#' className={`${obj.backtohome} ${obj.globalbtn}`}>BACK TO HOME</button>
     )
 }
 export function Button(){
     return(
-        <button onClick='#' className={obj.button}>BUTTON</button>
+        <button onClick='#' className={`${obj.button} ${obj.globalbtn}`}>BUTTON</button>
     )
 }
 export function About(){
     return(
-        <button onClick='#' className={obj.about}>ABOUT</button>
+        <button onClick='#' className={`${obj.about} ${obj.globalbtn}`}>ABOUT</button>
     )
 }
 export function Carriers(){
     return(
-        <button onClick='#' className={obj.carriers}>CARRIERS</button>
+        <button onClick='#' className={`${obj.carriers} ${obj.globalbtn}`}>CARRIERS</button>
     )
 }
