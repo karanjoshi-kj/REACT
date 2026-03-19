30d5c4c736295e1e86d06252d8db152d81322d31
karan joshi <kjkaranjoshi2006@gmail.com>
2026-03-19 18:35:38 +0530

    MULTIPLE PROPS IN REACT

---
+++ demo/src/components/STUDENTIDCARD/Multiidcard.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Details from "./Details";
+
+const Idcard = () => {
+  const studentid = {
+    image:"https://res.cloudinary.com/dafwtuvrd/image/upload/v1766646173/d9e34983-e718-4f50-9fb4-398feb05a545_nu3mtd.jpg",
+    name: "charanjeet singh sidhu",
+    address: "haldwani",
+    profession: "teacher",
+    salary: "5 L",
+
+    image:"https://res.cloudinary.com/dafwtuvrd/image/upload/v1766646173/d9e34983-e718-4f50-9fb4-398feb05a545_nu3mtd.jpg",
+    name: "charanjeet singh sidhu",
+    address: "haldwani",
+    profession: "teacher",
+    salary: "5 L",
+
+    image:"https://res.cloudinary.com/dafwtuvrd/image/upload/v1766646173/d9e34983-e718-4f50-9fb4-398feb05a545_nu3mtd.jpg",
+    name: "charanjeet singh sidhu",
+    address: "haldwani",
+    profession: "teacher",
+    salary: "5 L",
+  };
+  const { image, name, address, profession, salary } = studentid;
+  return (
+    <div>
+      <Details
+        img={image}
+        stname={name}
+        add={address}
+        sal={salary}
+        prof={profession}
+      />
+
+      {/*
+       <Details studentid={studentid} />
+      
+       THIS IS THE SECOND WAY TO PASS COMPLETE DATA 
+      
+      */}
+    </div>
+  );
+};
+
+export default Idcard;
