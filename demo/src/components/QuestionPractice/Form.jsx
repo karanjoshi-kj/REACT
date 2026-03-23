3ec19da4438633b0d840571fa724ef6a9a568a44
Karanjoshi-cse <karanjoshicse@gmail.com>
2026-03-23 18:26:03 +0530

    QUESTION PRATICE

--- demo/src/components/QuestionPractice/Form.jsx
+++ demo/src/components/QuestionPractice/Form.jsx
@@ -10,7 +10,21 @@ const Form = () => {
     };
   return (
     <div>
-      
+      <div className='container'>
+        <h2>Form</h2>
+
+        <input type='text' name='Name' placeholder='ENTER NAME'/><br></br>
+        <input type='email' name='Email' placeholder='ENTER EMAIL'/><br></br>
+        <input type='number' name='Age' placeholder='ENTER AGE'/><br></br>
+
+        <select name='Genader'>
+            <option value="">Select Gender</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+        </select><br></br>
+
+        <button>Submit</button>
+      </div>
     </div>
   )
 }
