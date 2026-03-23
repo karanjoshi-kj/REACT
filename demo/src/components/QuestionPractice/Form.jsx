1e45060367a29399031014dc7bf549916c6eea38
Karanjoshi-cse <karanjoshicse@gmail.com>
2026-03-23 18:18:24 +0530

    QUESTION PRATICE

---
+++ demo/src/components/QuestionPractice/Form.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Form = () => {
+
+    const formdata = {
+        Name:" ",
+        Email:" ",
+        Age:" ",
+        Gender:" "
+    };
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Form
