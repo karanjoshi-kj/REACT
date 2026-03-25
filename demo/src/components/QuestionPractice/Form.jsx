931478192ed45f0d9a276e58b88c97c917419e46
Karanjoshi-cse <kjkaranjoshi2006@gmail.com>
2026-03-25 18:57:04 +0530

    FORMS IN REACT

--- demo/src/components/QuestionPractice/Form.jsx
+++ demo/src/components/QuestionPractice/Form.jsx
@@ -1,52 +1,40 @@
 import React from "react";
+// import MutipleProps from "./MutipleProps";
 
 const Form = () => {
-  const formdata = {
-    Name: " ",
-    Email: " ",
-    Age: " ",
-    Gender: " ",
-  };
-
-  
-  const data = () => {
-    const name = document.getElementById("Name").value;
-    const email = document.getElementById("Email").value;
-    const age = document.getElementById("Age").value;
-    const gender = document.getElementById("Gender").value;
-
-    document.getElementById("output").innerHTML = `
-        <h1>SUBMITTED DATA</h1>
-        name : ${Name} </br>
-        email : ${Email} </br>
-        age : ${Age} </br>
-        gender : ${Gender} </br>
-        `;
-  };
+    const data = (e) => {
+        e.preventDefault();
+        const userData = new FormData(e.target);
+        const userdataentries = Object.fromEntries(userData.entries());
+        console.log("User Data:", userdataentries);
+        const userJSONData = JSON.stringify(userdataentries);
+        console.log(userJSONData)
+        // { userJSONData.map((index, student) => (<MutipleProps key={index} studentData={student} />)) }
+    };
 
 
-  return (
-    <div className="container">
-      <h2>Form</h2>
+    return (
+        <div className="container">
+            <h2>Form</h2>
+            <form onSubmit={data}>
+                <input type="text" id="Name" name="name" placeholder="ENTER NAME" required />
+                <br></br>
+                <input type="email" id="Email" name="email" placeholder="ENTER EMAIL" />
+                <br></br>
+                <input type="number" id="Age" name="number" placeholder="ENTER AGE" />
+                <br></br>
 
-      <input type="text" id="Name" placeholder="ENTER NAME" />
-      <br></br>
-      <input type="email" id="Email" placeholder="ENTER EMAIL" />
-      <br></br>
-      <input type="number" id="Age" placeholder="ENTER AGE" />
-      <br></br>
+                <select id="Gender" name="gender">
+                    <option value="">Select Gender</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                </select>
+                <br></br>
 
-      <select id="Gender">
-        <option value="">Select Gender</option>
-        <option value="Male">Male</option>
-        <option value="Female">Female</option>
-      </select>
-      <br></br>
-
-      <button onClick={data}>Submit</button>
-      <div id="output"></div>
-    </div>
-  );
+                <button type="submit">Submit</button>
+            </form>
+        </div >
+    );
 };
 
 export default Form;
