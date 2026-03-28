7e1a2c95959afb860ba45918f7775b5f6d0212b8
Karanjoshi-cse <kjkaranjoshi2006@gmail.com>
2026-03-28 17:10:31 +0530

    REACT HOOKS

---
+++ demo/src/components/HOOKS/MultiuseState.jsx
@@ -0,0 +1,16 @@
+//! THIS IS NOT A GOOD WAY TO PERFORM TASKS .
+//? Q. PRINT THE NAME ON THE BUTTON CLICK .
+
+
+
+import React , {useState} from 'react'
+const[username , setusername] = useState("");
+const MultiuseState = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MultiuseState
