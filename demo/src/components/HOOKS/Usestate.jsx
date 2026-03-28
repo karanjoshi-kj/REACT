11d9991ef61e2e13a5a989f6c30175b21cdc9eed
Karanjoshi-cse <kjkaranjoshi2006@gmail.com>
2026-03-28 16:54:43 +0530

    REACT HOOKS

--- demo/src/components/HOOKS/Usestate.jsx
+++ demo/src/components/HOOKS/Usestate.jsx
@@ -6,7 +6,6 @@ const Usestate = () => {
   return (
     <div>
         <button onClick={() => setcount(count + 1)}>CLICK ME TO UPDATE THE COUNTER</button>
-
         {/* TO PRINT THE VALUE IN THE FORMAT OF DOM . */}
         <h1>{count}</h1>    
     </div>
