5f0f9b0a67c421a0561b6f9e8bbb6326a910ee0a
Karanjoshi-cse <kjkaranjoshi2006@gmail.com>
2026-03-28 18:00:15 +0530

    REACT HOOKS

--- demo/src/components/HOOKS/FormuseState.jsx
+++ demo/src/components/HOOKS/FormuseState.jsx
@@ -5,19 +5,9 @@ const FormuseState = () => {
   return (
     <div>
         <form onSubmit={handlesubmit}>
-        
         <fieldset>
-
-
-
-
-
-
-
-
-
-
-
+            <legend>STUDENT REGISTRATION FORM :</legend>
+            <input type='text' name='Studentname' id='stname' placeholder='ENTER STUDENT NAME' onChange={handlechange} />
 
         </fieldset>
         </form>  
