01c8ae2b18749a49a4a742f3499701d86b0a8181
Karanjoshi-cse <kjkaranjoshi2006@gmail.com>
2026-03-28 17:22:55 +0530

    REACT HOOKS

--- demo/src/components/HOOKS/MultiuseState.jsx
+++ demo/src/components/HOOKS/MultiuseState.jsx
@@ -1,24 +1,28 @@
 //! THIS IS NOT A GOOD WAY TO PERFORM TASKS .
 //? Q. PRINT THE NAME ON THE BUTTON CLICK .
 
-
-
-import React , {useState} from 'react'
+import React, { useState } from "react";
 
 const MultiuseState = () => {
-    const[username , setusername] = useState("");
-const[displayname , setdisplayname] = useState("");
-function handlechange(){
+  const [username, setusername] = useState("");
+  const [displayname, setdisplayname] = useState("");
+  function handlechange() {
     setdisplayname(username);
-}
+  }
   return (
     <div>
-      <label htmlFor='name'>User Name :</label>
-      <input type='text' name='username' id='name' value={username} onChange={(e) => setusername(e.target.value)}/>
+      <label htmlFor="name">User Name :</label>
+      <input
+        type="text"
+        name="username"
+        id="name"
+        value={username}
+        onChange={(e) => setusername(e.target.value)}
+      />
       <button onClick={handlechange}>GET NAME</button>
       <p>{displayname}</p>
     </div>
-  )
-}
+  );
+};
 
-export default MultiuseState
+export default MultiuseState;
