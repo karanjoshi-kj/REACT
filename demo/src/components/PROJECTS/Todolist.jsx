b477302724e101fdd999f6f40de746bd81c3e695
karanjoshi-kj <kjkaranjoshi2006@gmail.com>
2026-03-31 17:09:03 +0530

    REACT PROJECT 1

--- demo/src/components/PROJECTS/Todolist.jsx
+++ demo/src/components/PROJECTS/Todolist.jsx
@@ -1,58 +1,77 @@
-import React , {useState} from 'react'
+import React, { useState } from "react";
 
 const Todolist = () => {
 
-  const[appdata , updatedata] = useState({task:""});
-  const[task , settask] = useState([]);             
-  
+  const [appdata, updatedata] = useState({ task: "" });
+  const [task, settask] = useState([]);
+
   const handlechange = (e) => {
-    const{name , value} = e.target;
+    const { name, value } = e.target;
 
     updatedata({
       ...appdata,
-      [name] : value
+      [name]: value
     });
   };
 
-  const handlesubmit = (e) =>{
-    e.preventdefault();
-    if(appdata.task === "")
-      return;
+  const handlesubmit = (e) => {
+    e.preventDefault();
 
-    settask([...task , appdata.task]);
-    updatedata({task : ""});
+    if (appdata.task === "") return;
+
+    settask([...task, appdata.task]);
+    updatedata({ task: "" });
   };
 
+  const deletetask = (index) => {   // index vo hai jo delete karna h 
+    let newtask = [];
 
-const deletetask = (index) => {
-  const newtask = [...task];  // tasks ka copy banaya
-  newtask.splice(index, 1);    // us index ka task delete
-  settask(newtask);           // state update
-};
-  c
+    for (let i = 0; i < task.length; i++) {
+      if (i !== index) {   // index ke alawa baaki sab task array m push kar diye . 
+        newtask.push(task[i]);
+      }
+    }
+
+    settask(newtask);
+  };
+
   return (
     <div>
 
       <form onSubmit={handlesubmit}>
         <fieldset>
           <legend>TO-DO LIST</legend>
-          <input type='text' name='task' placeholder='ENTER TASK' onChange={handlechange}/>
-          <button type='submit'>ADD</button>
+
+          <input
+            type="text"
+            name="task"
+            placeholder="ENTER TASK"
+            value={appdata.task}
+            onChange={handlechange}
+          />
+
+          <button type="submit">ADD</button>
+
         </fieldset>
       </form>
 
-<ul>
-      {task.length === 0 ? 
-      <p>NO TASK</p> : 
-      task.map((item , index)=>(
-        <li key={index}>
-          {item}
-          <button onClick={() => deletetask(index)}>DELETE</button>
-        </li>
-      ))
-      }
-</ul>
+      <ul>
+        {task.length === 0 ? (
+          <p>NO TASK</p>
+        ) : (
+          task.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button onClick={() => deletetask(index)}>
+                DELETE
+              </button>
+            </li>
+          ))
+        )}
+      </ul>
+
     </div>
-  )
-}
-export default Todolist
+  );
+};
+
+export default Todolist;
