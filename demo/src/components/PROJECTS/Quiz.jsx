63ca42ff1eddd2bbddb74d84dce8dcb8beaf8f06
karanjoshi-kj <kjkaranjoshi2006@gmail.com>
2026-04-03 13:33:18 +0530

    PROJECT 2

--- demo/src/components/PROJECTS/Quiz.jsx
+++ demo/src/components/PROJECTS/Quiz.jsx
@@ -6,8 +6,7 @@ const Quiz = () => {
     const[select , setselect] = useState("");
     const[score , setscore] = useState(0);
     const nextques = () =>{
-
-      
+  
         if (index < data.Questions.length) {
             setindex(index + 1)
         }
