e005e5f2a645e9575adc8c472f831e07f3e78945
karanjoshi-kj <kjkaranjoshi2006@gmail.com>
2026-04-03 13:43:33 +0530

    PROJECT 2

--- demo/src/components/PROJECTS/Quiz.jsx
+++ demo/src/components/PROJECTS/Quiz.jsx
@@ -1,39 +1,50 @@
-import React , {useState} from 'react';
+import React, { useState } from "react";
 import data from "./Questions.json";
 
 const Quiz = () => {
-    const[index , setindex] = useState(0);
-    const[select , setselect] = useState("");
-    const[score , setscore] = useState(0);
-    const nextques = () =>{
-        if (index < data.Questions.length) {
-            setindex(index + 1)
-        }
+  const [index, setindex] = useState(0);
+  const [select, setselect] = useState("");
+  const [score, setscore] = useState(0);
+  const nextques = () => {
+    if (setselect === "") {
+      alert("PLZZ SELECT AN OPTION");
+      return;
     }
 
-    const prevques = () => {
-      if (index > 0) { 
-        setindex(index-1)
-      }  
+    if (setselect === data.Questions[index].answer) {
+      setscore(score + 1);
     }
+
+    if (index < data.Questions.length - 1) {
+      setindex(index + 1);
+      setselect("");
+    } else {
+      alert(
+        `YOUR SCORE : ${score + select === data.Questions[index].answer ? 1 : 0}`,
+      );
+    }
+  };
+
+  const prevques = () => {
+    if (index > 0) {
+      setindex(index - 1);
+    }
+  };
 
   return (
     <div>
       <h1>{data.Questions[index].question}</h1>
-      {
-        data.Questions[index].options.map((opt , i)=>(
-            // <p key={i}>{opt}</p>
-            <div>
-            <input type='radio' key={i} name='answersbutton' value={opt} /> 
-            <span>{opt}</span>
-            </div>
-        ))
-      }
+      {data.Questions[index].options.map((opt, i) => (
+        // <p key={i}>{opt}</p>
+        <div>
+          <input type="radio" key={i} name="answersbutton" value={opt} />
+          <span>{opt}</span>
+        </div>
+      ))}
       <button onClick={nextques}>NEXT PROBLEM</button>
       <button onClick={prevques}>PREVIOUS PROBLEM</button>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Quiz
+export default Quiz;
