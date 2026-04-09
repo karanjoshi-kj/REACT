1205b165704df1fc1c49c62271c313a5e1dbcb22
karanjoshi-kj <kjkaranjoshi2006@gmail.com>
2026-04-09 19:17:38 +0530

    USEEFFECT IN REACT

--- demo/src/components/USEEFFECT/Useeffect.jsx
+++ demo/src/components/USEEFFECT/Useeffect.jsx
@@ -3,6 +3,10 @@ import React , {useState , Useeffect} from 'react'
 const Useeffect = () => {
     const[counter , setcounter] = useState(0);
     const[timer , settimer] = useState(0);
+
+    function helloprint(){
+        console.log("print hello");
+    }
   return (
     <div>
       
