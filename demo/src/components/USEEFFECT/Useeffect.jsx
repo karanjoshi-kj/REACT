02421f9fabe3fd3504d01d9ade917000cba28feb
karanjoshi-kj <kjkaranjoshi2006@gmail.com>
2026-04-09 19:21:53 +0530

    USEEFFECT IN REACT

--- demo/src/components/USEEFFECT/Useeffect.jsx
+++ demo/src/components/USEEFFECT/Useeffect.jsx
@@ -1,3 +1,22 @@
+
+//! SYNTAX
+//! useState: data to ko store with update values
+//! useEffect : mainly used to resolve the side effects in the code with various parameters
+
+
+//? useEffect(() => {
+
+//? }, dependencies)
+
+
+
+
+
+
+
+
+
+
 import React , {useState , Useeffect, useEffect} from 'react'
 
 const Useeffect = () => {
@@ -8,11 +27,14 @@ const Useeffect = () => {
         console.log("print hello");
     }
     useEffect(()=>{
-        helloprint();
-    })
+        helloprint();  // NOT INDEPENDENT IN NATURE DUE TO USEEFFECT 
+    } , [])
+
+    helloprint();   //THIS IS INDEPENDENT IN NATURE
   return (
     <div>
-      
+      <button onClick={() => setcounter(counter+1)}>COUNTER {counter}</button>
+      <button onClick={() => settimer(timer+1)}>TIMER {timer}</button>
     </div>
   )
 }
