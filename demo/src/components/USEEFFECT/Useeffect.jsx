3abeb63d7d7423f7693f423192d5488bf8abc78e
karanjoshi-kj <kjkaranjoshi2006@gmail.com>
2026-04-10 18:24:31 +0530

    USEEFFECT IN REACT

--- demo/src/components/USEEFFECT/Useeffect.jsx
+++ demo/src/components/USEEFFECT/Useeffect.jsx
@@ -24,7 +24,7 @@ const UseEffectDemo = () => {
         helloPrint();
     }, [])
 
-    // helloPrint(); //independent in nature
+    // helloPrint();  independent in nature
     return (
         <div>
             {/* Hello */}
