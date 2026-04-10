fca9f2b1fba2d3f7e9f3486a6d76381591183a5b
karanjoshi-kj <kjkaranjoshi2006@gmail.com>
2026-04-10 18:39:34 +0530

    USEEFFECT IN REACT

---
+++ demo/src/components/USEEFFECT/Statedependency.jsx
@@ -0,0 +1,14 @@
+import React , {useState , useEffect} from 'react'
+
+const Statedependency = () => {
+     const [counter, setCounter] = useState(0);
+        const [timer, setTimer] = useState(0);
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Statedependency
