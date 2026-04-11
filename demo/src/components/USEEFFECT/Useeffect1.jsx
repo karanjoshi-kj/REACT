fbc151585139fc5166deea7fc85f5c2af07bff01
karanjoshi-kj <kjkaranjoshi2006@gmail.com>
2026-04-11 14:09:33 +0530

    USE EFFECT IN REACT

--- demo/src/components/USEEFFECT/Useeffect1.jsx
+++ demo/src/components/USEEFFECT/Useeffect1.jsx
@@ -1,6 +1,12 @@
 import React , {useState , Useeffect} from 'react'
 
 const Useeffect1 = () => {
+  const questiontiming = 5
+  const [counter , setcountet] = useState(1);
+  const [timing , setTiming] = useState(questiontiming);
+
+  
+
   return (
     <div>
       
@@ -9,3 +15,55 @@ const Useeffect1 = () => {
 }
 
 export default Useeffect1
+
+// import React, { useEffect, useState } from 'react'
+
+// const UseEffectDemo = () => {
+//   
+//     useEffect(() => {
+//         setTiming(questiontimings); //it will update the value of timer to 10 whenever called
+//         const interval = setInterval(() => {
+//             setTiming((timing) => {
+//                 if (timing === 0) {
+//                     setCounter(counter + 1)
+//                     return 0;
+//                 }
+//                 return timing - 1;
+//             });
+//         }, 1000);
+//         return () => clearInterval(interval);
+//     }, [counter]);
+
+
+//     const nextbutton = () => {
+//         setCounter(() => counter + 1);
+//     };
+//     return (
+//         <div>
+//             <h1 >Counter : {counter}</h1>
+//             <h2>Time  Left {timing} seconds </h2>
+//             <button onClick={nextbutton}>Next </button>
+//         </div >
+//     )
+// }
+// export default UseEffectDemo;
+
+// const [timer, setTimer] = useState(10);
+{/* <button onClick={() => setTimer(timer + 3)}>Timer{timer}</button> */ }
+{/* Hello */ }
+
+// function helloPrint() {
+//     console.log(" Print Hello");
+// }
+// useEffect(() => {
+//     helloPrint();
+// }, [counter, timer])
+
+// helloPrint(); //independent in nature
+// useState: data to ko store with update values
+// useEffect : mainly used to resolve the side effects in the code with various parameters
+
+
+// useEffect(() => {
+
+// }, dependencies)
