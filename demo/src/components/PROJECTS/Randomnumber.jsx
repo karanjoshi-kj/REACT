8d7b082239afb3c93dc38481109ca4b4f890ee28
karanjoshi-kj <kjkaranjoshi2006@gmail.com>
2026-04-14 17:28:20 +0530

    PROJECT 3 : GENERATE RANDOM NUMBER

--- demo/src/components/PROJECTS/Randomnumber.jsx
+++ demo/src/components/PROJECTS/Randomnumber.jsx
@@ -3,6 +3,10 @@ import React , {useState , useEffect} from 'react'
 const Randomnumber = () => {
     const [number , setnumber] = useState(null);
     const [history , sethistory] = useState([]);
+
+    const generatenum = () =>{
+        const randomnum = Math.floor(Math.random() * 1000) + 1 ;
+    }
   return (
     <div>
       <button>GENERATE NUMBER</button>
