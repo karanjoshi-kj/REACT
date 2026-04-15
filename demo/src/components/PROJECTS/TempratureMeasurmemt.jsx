619f1d0287b95cc5b83ea7ec6a206df886426d7c
karanjoshi-kj <kjkaranjoshi2006@gmail.com>
2026-04-15 18:12:25 +0530

    PROJECT 4 : TEMPRATUE MEASURMENT

--- demo/src/components/PROJECTS/TempratureMeasurmemt.jsx
+++ demo/src/components/PROJECTS/TempratureMeasurmemt.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React , {useState , useEffect} from 'react'
 
 const TempratureMeasurmemt = () => {
+    const[temp , settemp] = useState("");
+    const[msg , setmsg] = useState("");
   return (
     <div>
       
