1e817c28091fb4f003c933110015a3f869f8920b
karanjoshi-kj <kjkaranjoshi2006@gmail.com>
2026-04-16 18:41:13 +0530

    PROJECT 5 : form validation

--- demo/src/components/PROJECTS/FormValidation.jsx
+++ demo/src/components/PROJECTS/FormValidation.jsx
@@ -7,6 +7,26 @@ const FormValidation = () => {
         email : "",
         password : ""
     });
+
+   const updation = () => {
+    if (form.name === "") {
+        alert("NAME IS REQUIRED")
+    }
+
+    if (form.email === "") {
+        alert("EMAIL IS REQUIRED");
+    }
+    else if (!form.email.includes("@")) {
+        alert("RE-ENTER THE EMAIL");
+    }
+
+    if (form.password === "") {
+        alert("ENTER THE PASSWORD");
+    }
+    else if (form.password.length < 6) {
+        alert("PASSWORD SHOULD BE GREATER THEN SIX DIGITS");
+    }
+   }
   return (
     <div>
 <form>
@@ -20,6 +40,8 @@ const FormValidation = () => {
     <input type='password' name='password' /><br></br>
    
    <button type='submit'>SUBMIT</button>
+
+   <updation/>
 </form>
     </div>
   )
