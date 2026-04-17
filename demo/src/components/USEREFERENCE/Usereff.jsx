33b65859aadde23f1748bbe294b282f765063834
karanjoshi-kj <kjkaranjoshi2006@gmail.com>
2026-04-17 18:19:04 +0530

    PROJECT 5 : FORM VALIDATION PROJECT

--- demo/src/components/USEREFERENCE/Usereff.jsx
+++ demo/src/components/USEREFERENCE/Usereff.jsx
@@ -5,11 +5,12 @@ const Usereff = () => {
     const[couter , setcounter] = useState(1);
     const counter1 = useRef(1);
     const handlechange = () => {
-        
+        counter1.current++;
+        console.log(counter1.current);
     }
   return (
     <div>
-      <button onClick={()=> setcounter(counter + 1)}></button>
+      <button onClick={()=> setcounter(counter + 1)}>Counter : {counter}</button>
       <button onClick={handlechange}>counter11 : {counter1.current}</button>
     </div>
   )
