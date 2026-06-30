f0cea180bcec76db8bec7efe6e425ecfc4dbc4af
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2026-06-30 09:54:26 +0530

    REACT HELMETS

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,81 +1,30 @@
-// import TopBar from "./components/Topbar/topbar"
-// import NavBar from "./components/Topbar/NavBar"
-// import Topbar2 from "./components/TopBar2/Topbar2"
-
-// import Quiz from "./components/PROJECTS/Quiz"
-// import Randomnumber from "./components/PROJECTS/Randomnumber"
-// import FormValidation from "./components/PROJECTS/FormValidation"
-import Usereff from "./components/USEREFERENCE/Usereff"
-// import TempratureMeasurmemt from "./components/PROJECTS/TempratureMeasurmemt"
-// import Statedependency from "./components/USEEFFECT/Statedependency"
-// import Useeffect1 from "./components/USEEFFECT/Useeffect1"
-// import Useeffect from "./components/USEEFFECT/Useeffect"
-// import Todolist from "./components/PROJECTS/todolist"
-
-// import FormuseState from "./components/HOOKS/FormuseState"
-// import MultiuseState from "./components/HOOKS/MultiuseState"
-// import StringsuseState from "./components/HOOKS/StringsuseState"
-// import Usestate from "./components/HOOKS/Usestate"
-// import Form from "./components/QuestionPractice/Form"
-
-// import Navbar3 from "./components/navbar4/Navbar3"
-
-// import Navbar7 from "./components/navbar7/Navbar7"
-// import Buttonlist from "./components/question/Buttonlist"
-// import Form from "./components/MULTIPROPS/Form";
-// import Nav from "./components/props/Nav";
-// import Form from "./components/MULTIPROPS2/Form";
-// import Idcard from "./components/STUDENTIDCARD/Idcard";
-
-// //! DESTRUCTURING OF THE FUNCTIONAL COMPONENTS DURING IT'S CALLING .
-// import { Backbutton , Backtotopbutton } from "./components/buttons/Button.jsx"
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import HelmetComponent from './components/Helmets/HelmetComponent'
 
 
-function App() {
+const App = () => {
   return (
-    <Usereff/>
-    // <FormValidation/>
-    // <><TempratureMeasurmemt/>
-    // {/* <Useeffect1/> */}
-  //   {/* <Statedependency/> */}
-  //   {/* <Useeffect/> */}
-  //   {/* <Quiz/> */}
-  //   {/* <Todolist/> */}
-  //   {/* <FormuseState/> */}
-  //   {/* <MultiuseState/> */}
-  //   {/* <StringsuseState/> */}
-  //   {/* <Usestate/> */}
-  //   {/* <Form/> */}
-  //   {/* <Form/> */}
-  // {/* <Idcard/> */}
+       <HelmetProvider>
+      <Router>
+        <Helmet>
+          <title>Default- Coaching App</title>
+          <meta name="description" content="Coaching application" />
+        </Helmet>
+        <nav>
+          <Link to="/">Home<HelmetComponent /></ Link><br/>
+          <Link to="https://nlp.nexterp.in/nlp/nlp/v1/home/collaboration-dashboard">About | ON WEBSITE </Link><br/>
+          <a href="https://nlp.nexterp.in/nlp/nlp/v1/home/collaboration-dashboard" target="_blank" rel="noopener noreferrer">External Link  |ERP  </a>
+        </nav>
+        <Routes>
+          <Route path="/" element={<HelmetComponent />} />
+          {/* <Route path="*" element={<Error404 />} /> */}
+        </Routes>
+      </Router>
+    </HelmetProvider>
 
-
-
-// {/* 
-//      <Nav name="Karan Joshi" />
-//       <Nav name="Shubham" />
-//       <Nav name="Rahul" />
-//       <Nav name="Charanjeet Sing" />
-//       <Nav name="Karan" /> */}
-//       {/*PARENT COMPONENT IS CALLING THE CHILD COMPONENT  */}
-
-
-//     {/* <Buttonlist/> */}
-//     {/* <Backbutton/>
-//     <Backtotopbutton/> */}
-//     {/* <Navbar7 /> */}
-//     {/* <Navbar6 /> */}
-//     {/* <Navbar5 /> */}
-//     {/* <Navbar2 />       */}
-//       {/* <Topbar2 /> */}
-//       {/* <NavBar /> */}
-//     </>
   )
 }
 
-// EXPORT DEFAULT FUNCTION NAME . 
-export default App  
-
-
-// exporrt default function name ==> 
-  // export default function App() {
+export default App
---
+++ demo/src/components/Helmets/HelmetComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelmetComponent = () => {
+  return (
+    <p>Hello, this is a Helmet component.</p>
+  );
+};
+
+export default HelmetComponent;
