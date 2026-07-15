7f86c1aa117589de35c7488edaf779eef720923b
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2026-07-15 21:55:14 +0530

    QUESTION PRATICE ON REACT HELMET

---
+++ demo/src/components/Helmets/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Titlefile1} from '../Common/Titlefile1'
+const Services = () => {
+  return (
+    <div>
+      <Titlefile1 Titlecopy="Services" />
+      <h1>Our Services</h1>
+    </div>
+  )
+}
+
+export default Services
