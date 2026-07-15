11c895acbb83595405687be4bc4b89e46862085f
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2026-07-15 21:51:46 +0530

    QUESTION PRATICE ON REACT HELMET

---
+++ demo/src/components/Common/Titlefile1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Titlefile1 = ({ Titlecopy}) => {
+  return (
+    <div>
+      <helmet>
+        <title>{Titlecopy}</title>
+      </helmet>
+    </div>
+  )
+}
+
+export default Titlefile1
