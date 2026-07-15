1452ad2fcc3e2fd432f77510ee0c6d71689c3cbb
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2026-07-15 21:54:24 +0530

    QUESTION PRATICE ON REACT HELMET

---
+++ demo/src/components/Helmets/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Titlefile1} from '../Common/Titlefile1'
+
+const About = () => {
+  return (
+    <div>
+     <Titlefile1 Titlecopy="About Us" />
+      <h1>About Us</h1>
+    </div>
+  )
+}
+
+export default About
