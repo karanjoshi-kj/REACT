a14fa6184c67370dc2c1220013bcd6ea94503776
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2026-07-16 08:56:45 +0530

    META TAGS

---
+++ demo/src/components/Helmets/Meta.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Helmet } from 'react-helmet-async'
+const Meta = (data) => {
+    const{description, title , keyword} = data
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keyword" content={keyword} />
+    </Helmet>
+  )
+}
+
+export default Meta
