881f9fc3451cd7a91bfa505c36be31fc3779030f
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2026-07-18 17:54:43 +0530

    QUESTION PRATICE

---
+++ demo/src/components/Common/Underconstruction.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Underconstruction = () => {
+  return (
+    <div>
+      <h1>PAGE UNDER CONSTRUCTION</h1>
+    </div>
+  )
+}
+
+export default Underconstruction
