1bb677740979a2b89c5427df34eb85ba137d28d7
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2026-07-18 17:50:28 +0530

    QUESTION PRATICE

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Question from './components/navbar1/Question';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'; // <-- BrowserRouter add kiya
 import Aboutus from './components/pages/Aboutus';
-
+import Services from './components/Common/Pagenotfound';
 const App = () => {
   return (
     <BrowserRouter> {/* <-- Yahan start kiya */}
       <Routes>
         <Route path="/" element={<Question />} />
         <Route path="/about" element={<Aboutus />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
     </BrowserRouter> 
   );
