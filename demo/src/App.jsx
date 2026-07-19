5e8fa7f49748a7d6ac3a8e20bf56eccb1e113add
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2026-07-19 09:12:23 +0530

    MAP FUNCTION

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,22 +1,12 @@
-import React from 'react';
-import Question from './components/navbar1/Question';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'; // <-- BrowserRouter add kiya
-import Aboutus from './components/pages/Aboutus';
-import Services from './components/Common/Pagenotfound';
-import Contact from './components/Common/Underconstruction';
+import React from 'react'
+import Mapfun from './components/mapfunction/Mapfun';
 
 const App = () => {
   return (
-    <BrowserRouter> {/* <-- Yahan start kiya */}
-      <Routes>
-        <Route path="/" element={<Question />} />
-        <Route path="/about" element={<Aboutus />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-        
-      </Routes>
-    </BrowserRouter> 
-  );
-};
+    <div>
+      <Mapfun />
+    </div>
+  )
+}
 
-export default App;
+export default App
--- demo/src/components/mapfunction/Mapfun.jsx
+++ demo/src/components/mapfunction/Mapfun.jsx
@@ -5,11 +5,19 @@ const Mapfun = () => {
   return (
     <div>
       <h1>Student Names</h1>
-      {   //! YE BRACKET ISLIYE AAYA H KYOKI REACT M DIRECTLY JS NAHI CHLA SAKTE HAI ....
+      {/* {   //! YE BRACKET ISLIYE AAYA H KYOKI REACT M DIRECTLY JS NAHI CHLA SAKTE HAI ....
       studentnames.map((name)=>{
         console.log(name);     //? THIS WILL PRINT THE OUTPUT ON THE CONCOLE ..... 
       })
+    } */}
+
+
+    {
+        studentnames.map((name)=>{
+            
+        })
     }
+
     </div>
   )
 }
