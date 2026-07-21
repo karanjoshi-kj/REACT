14b41384a9c2feb9b455f8ff9c21b963473f5ce1
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2026-07-21 18:52:07 +0530

    QUESTION PRATICE

--- demo/src/components/mapfunction/Card.jsx
+++ demo/src/components/mapfunction/Card.jsx
@@ -4,24 +4,26 @@ const Card = () => {
     const project = [
         {
         image : "https://res.cloudinary.com/dafwtuvrd/image/upload/v1766646173/d9e34983-e718-4f50-9fb4-398feb05a545_nu3mtd.jpg",
-        // tag : "",
-        // tagline : "",
-        // description : "",
-        // developer : "karan",
-        // livelink : "",
-        // githublink : ""
+        tag : "",
+        tagline : "",
+        description : "",
+        developer : "",
+        livelink : "",
+        githublink : ""
     },
 ];
   return (
     <div className="container">
         {project.map((abc)=>(
+            <div>
             <img src={abc.image} alt={abc.tagline} />
-            // <span className="tag">{abc.tag}</span>
-            // <h2>{abc.tagline}</h2>
-            // <p>{abc.description}</p>
-            // <h3>{abc.developer}</h3>
-            // <a href={abc.livelink} target="_blank" rel="notfound">LIVE HOSTING</a>
-            // <a href={abc.livelink} target="_blank" rel="notfound">LIVE HOSTING</a>
+            <span className="tag">{abc.tag}</span>
+             <h2>{abc.tagline}</h2>
+             <p>{abc.description}</p>
+             <h3>{abc.developer}</h3>
+             <a href={abc.livelink} target="_blank" rel="notfound">LIVE HOSTING</a>
+             <a href={abc.githublink} target="_blank" rel="notfound">GITHUB LINK</a>
+        </div>     
         ))}
     </div>     
   )
