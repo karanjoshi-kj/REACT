d3edf9ad3df5a091cd27af56501cd8690f30de8b
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2026-07-23 17:43:19 +0530

    QUESTION PRATICE

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 // import Mapfun from './components/mapfunction/Mapfun';
 // import Keyattri from './components/mapfunction/Keyattri';
 // import QuestionPratice from './components/mapfunction/QuestionPratice';
-import Card from './components/mapfunction/Card';
+// import Card from './components/mapfunction/Card';
+import Filter from './components/FILTERS/Filter';
 
 const App = () => {
   return (
     <div>
-      <Card />
+      <Filter />
+      {/* <Card /> */}
       {/* <Mapfun /> */}
       {/* <Keyattri /> */}
       {/* <QuestionPratice /> */}
---
+++ demo/src/components/FILTERS/Filter.jsx
@@ -0,0 +1,16 @@
+//? FILTER RETURNS THE OUTPUT IN THE FORM OF ARRAY .
+
+import React from 'react'
+
+const Filter = () => {
+    const numbers = [1,2,3,4,5,6];
+    const evenno = numbers.filter((num)=> num%2==0);
+    // console.log(evenno);
+  return (
+    <div>
+      <h1>EVEN NUMBERS{evenno}</h1>
+    </div>
+  )
+}
+
+export default Filter
