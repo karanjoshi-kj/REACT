4745b6e37fdd8efa99c873320e9ec68734e8882b
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2026-08-31 18:08:51 +0530

    VARAIBLE SHADOWING IN JS

---
+++ demo/src/components/SCOPING/Test.js
@@ -0,0 +1,13 @@
+function x(){
+    let a = 10;
+    function y(){
+        console.log("FUNCTION Y IS CALLED");
+        console.log(a);  //?10
+    }
+    return y ;  //! FUNCTION CALL NAHI HUI H 
+    console.log("FUNCTION X IS CALLED");
+}
+console.log("GLOBAL FUNCTION");
+let res = x();  //! X KO CALL KIYA H  
+// res();
+console.log("FUNCTION IS CALLED");
