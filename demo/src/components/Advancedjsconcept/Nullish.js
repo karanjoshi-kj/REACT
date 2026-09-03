914de42f385179e22a1c41bddcd9d5164775a4f8
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2026-09-03 17:54:28 +0530

    UPDATED

---
+++ demo/src/components/Advancedjsconcept/Nullish.js
@@ -0,0 +1,12 @@
+const user={
+    "name":"karan",
+    "age":25,
+    marks:"",
+
+    // "course":null,
+}
+
+// Nullish operator will run the false condition only in 2 parameters: null, undefined
+// OR || will run in various  : false , 0, empty String, null, undefined , NaN
+console.log(user.marks||"AB")
+console.log(user.marks??"AB")
