4770d7086e4e366d20db0d560253d90581e90ccc
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2026-09-10 17:50:50 +0530

    HOISTING

---
+++ demo/src/components/Advancedjsconcept/Closure.js
@@ -0,0 +1,30 @@
+//! CLOSURE IN JS 
+
+//? CLOSURE FUNCTION M CHILD FUNCTION APNI PURI DEFINATION KO PARENT FUNCTION KO RETURN KARTA HAI .
+//? PARENT FUNCTIONS KE SAREEI BODY (VARIABLES , FUNCTIONS)  SABB YADD RAKHTA HAI .
+
+
+//!  BASIC SNIPET
+function x(){
+    var b = 10;
+    function y (){
+        console.log(b);
+    }
+    y();
+}
+x();
+
+
+
+function inc(){
+    var a = 10 ;
+    function y(){
+        a++;
+        console.log(a);
+    }
+    return y;
+}
+const cc = inc();
+cc();
+cc();
+cc();
